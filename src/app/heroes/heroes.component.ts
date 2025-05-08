5ef90c5fa47fd27beae0280af6e0c8c60a1e7dbb
Yusufulla Qureshi <y.qureshi@andersenlab.com>
2025-05-08 15:30:23 +0530

    created hero and hero-detail components. added hero service to get mock heroes data in an async behaviour using observables

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '@models/hero.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '@services/hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule, FormsModule, HeroDetailComponent],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css',
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  removeSelectedHero(): void {
+    this.selectedHero = undefined;
+  }
+}
