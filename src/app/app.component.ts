66fc97cb46298cecf9052b69bb17e816553cd86e
Yusufulla Qureshi <y.qureshi@andersenlab.com>
2025-05-09 19:09:55 +0530

    created dashboard component and added routes and navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { HeroesComponent } from '@components/heroes/heroes.component';
 import { CommonModule } from '@angular/common';
 import { MessagesComponent } from '@components/messages/messages.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroesComponent, CommonModule, MessagesComponent],
+  imports: [CommonModule, MessagesComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from '@components/dashboard/dashboard.component';
+import { HeroDetailComponent } from '@components/hero-detail/hero-detail.component';
+import { HeroesComponent } from '@components/heroes/heroes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: '**', redirectTo: 'dashboard' },
+];
