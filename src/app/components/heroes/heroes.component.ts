7b3fee1232f05796784e8c56af58141bf666161c
Yusufulla Qureshi <y.qureshi@andersenlab.com>
2025-05-12 20:20:25 +0530

    created in-memory-service, data interaction for adding, updating and deleting heroes using HttpClient

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -3,11 +3,12 @@ import { Hero } from '@models/hero.model';
 import { CommonModule } from '@angular/common';
 import { HeroService } from '@services/hero.service';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
@@ -22,5 +23,21 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this._heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  addHero(name: string): void {
+    name = name.trim();
+    if (!name) return;
+
+    const newHero: Hero = { name } as Hero;
+
+    this._heroService.addHero(newHero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  deleteHero(hero: Hero): void {
+    this.heroes = this.heroes.filter((currHero) => currHero.id !== hero.id);
+    this._heroService.deleteHero(hero.id).subscribe();
   }
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -2,24 +2,73 @@ import { Injectable } from '@angular/core';
 import { Hero } from '@models/hero.model';
 import { HEROES } from '@mocks/mock-heroes';
 
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { MessagesService } from './messages.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private _messagesService: MessagesService) {}
+  private heroesUrl = 'api/heroes';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(
+    private _messagesService: MessagesService,
+    private _http: HttpClient
+  ) {}
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this._messagesService.add('Heroes: fetched heroes');
-    return heroes;
+    return this._http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this._log('fetched heroes')),
+      catchError(this._handleError<Hero[]>('getHeroes', []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find((h) => h.id === id)!;
-    this._messagesService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    return this._http.get<Hero>(`${this.heroesUrl}/${id}`).pipe(
+      tap((_) => this._log(`fetched hero id=${id}`)),
+      catchError(this._handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this._http.post<Hero>(this.heroesUrl, hero).pipe(
+      tap((_) => this._log(`Hero ${hero.name} added!`)),
+      catchError(this._handleError<Hero>('addHero'))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this._http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((_) => this._log(`updated hero id=${hero.id}`)),
+      catchError(this._handleError<any>('updateHero'))
+    );
+  }
+
+  deleteHero(id: number): Observable<Hero> {
+    return this._http
+      .delete<Hero>(`${this.heroesUrl}/${id}`, this.httpOptions)
+      .pipe(
+        tap((_) => this._log(`Hero ${id} delete!`)),
+        catchError(this._handleError<Hero>('deleteHero'))
+      );
+  }
+
+  private _log(message: string) {
+    this._messagesService.add(`HeroService: ${message}`);
+  }
+
+  private _handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this._log(`${operation} failed: ${error.body.error}`);
+
+      return of(result as T);
+    };
   }
 }
